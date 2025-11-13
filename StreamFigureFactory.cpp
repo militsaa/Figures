3e2b11b2cd518dfc8501011ac88c00f6aae21334
Militsa <militsa.lazarova05@gmail.com>
2025-11-13 21:42:18 +0200

    StreamFactory

--- StreamFigureFactory.cpp
+++ StreamFigureFactory.cpp
@@ -1,6 +1,12 @@
 #include "StreamFigureFactory.h"
+#include "StringToFigure.h"
+
+StreamFigureFactory::StreamFigureFactory(std::istream& input) :in(input) {}
 
 Figure* StreamFigureFactory::create()
 {
-    return nullptr;
+    std::string line;
+    if (!std::getline(in, line))
+        throw std::runtime_error("Input stream is not in a good state.");
+    return StringToFigure::stringToFigure(line);
 }
--- StreamFigureFactory.h
+++ StreamFigureFactory.h
@@ -1,6 +1,8 @@
 #include "FigureFactory.h"
-
+#include <iostream>
 class StreamFigureFactory : public FigureFactory {
+    std::istream& in;
 public:
+    StreamFigureFactory(std::istream& input);
     virtual Figure* create() override;
 };
